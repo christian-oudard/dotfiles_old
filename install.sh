a88ea8692b54935dfcf109745e019dd0b8e29097
Christian Oudard <christian.oudard@gmail.com>
2009-10-23 09:06:35 -0400

    Made the install script not link itself to the home directory.

--- install.sh
+++ install.sh
@@ -3,11 +3,12 @@
 TARGET_DIR=$HOME
 SCM="git"
 
-# link all versioned files to the home directory, asking about overwrites
+# Run from the script directory.
 cd $(dirname $0)
 script_dir=$(pwd)
 script_name=$(basename $0)
 
+# Ask the source control for a list of all the versioned files.
 case $SCM in
 "git")
 	files=$(git ls-tree --name-only HEAD);;
@@ -19,8 +20,10 @@ case $SCM in
 	;;
 esac
 
+# Link all versioned files to the home directory, asking about overwrites.
+# Don't link this install script.
 cd $TARGET_DIR
 for file in $files; do
+	if [ "$file" = "$script_name" ] ; then continue; fi # Don't link this script.
 	ln --symbolic --interactive $script_dir/$file
 done
-rm $TARGET_DIR/$script_name
