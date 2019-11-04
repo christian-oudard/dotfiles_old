70ec9a64a8b9a387e82bab0720f077c6dabf3903
Christian Oudard <christian.oudard@gmail.com>
2019-11-04 15:56:17 -0800

    Add signal install script.

---
+++ scripts/install_signal.sh
@@ -0,0 +1,4 @@
+sudo apt install curl
+curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+sudo apt update && sudo apt install signal-desktop
