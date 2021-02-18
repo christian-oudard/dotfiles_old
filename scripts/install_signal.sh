184018595560e90bbed7f95767275a06b83900d2
Christian Oudard <christian.oudard@gmail.com>
2021-02-18 13:58:38 -0800

    bashrc improvements and misc
    bashrc:
        Use sudo with docker.
        Use human readable file sizes with ls aliasse.
        Add cargo env.
    
    Add signal desktop install script for linux.

--- scripts/install_signal.sh
+++ scripts/install_signal.sh
@@ -1,4 +1,12 @@
-sudo apt install curl
-curl -s https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+#!/bin/sh
+
+sudo apt install -y curl wget
+
+# 1. Install our official public software signing key
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+
+# 2. Add our repository to your list of repositories
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+
+# 3. Update your package database and install signal
 sudo apt update && sudo apt install signal-desktop
